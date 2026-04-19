f72c5b62ccf3613c8115742e2dec0cb2574346a1
fayolah <earlbro0423@gmail.com>
2026-04-19 09:46:17 -0400

    removed tierlist page and went for single page layout

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  // Your configuration options here
-  base:"/movie-tracker",
-  tierlist:"tierlist.html"
-})
+  base: "/movie-tracker/"
+});
